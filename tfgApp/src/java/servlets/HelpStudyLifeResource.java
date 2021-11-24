902843b6d51c26481529a0834e759dad79faa784
David Recio Arnés <davidrecioarnes@gmail.com>
2021-11-24 14:32:36 +0100

    sx

--- tfgApp/src/java/servlets/HelpStudyLifeResource.java
+++ tfgApp/src/java/servlets/HelpStudyLifeResource.java
@@ -47,8 +47,8 @@ public class HelpStudyLifeResource {
        Scraper scrp= new Scraper();
 
 
-       http="https://www.descubremadrid.com/listado-completo-de-todas-las-universidades-publicas-y-privadas-en-madrid/#Universidades_Publicas_en_Madrid";
-       
+//       http="https://www.descubremadrid.com/listado-completo-de-todas-las-universidades-publicas-y-privadas-en-madrid/#Universidades_Publicas_en_Madrid";
+       http="https://www.comunidad.madrid/servicios/educacion/localizacion-universidades-madrilenas";
       ArrayList etiquetasElementos = new ArrayList();
         ArrayList tiposElementos = new ArrayList();
                 ArrayList separadores= new ArrayList();
@@ -57,31 +57,42 @@ public class HelpStudyLifeResource {
                  ArrayList<String> profundidadElemento1 = new ArrayList();
                   ArrayList<String> profundidadElemento2 = new ArrayList();
                    ArrayList<String> profundidadElemento3 = new ArrayList();
-                 
-       etiquetasElementos.add("h3");
+          
+                   
+                
+       etiquetasElementos.add("h2");
        tiposElementos.add("tag");
        separadores.add("Universidad");
-       profundidadElemento1.add("div.entry-content");
-       
-        etiquetasElementos.add("li");
-       tiposElementos.add("tag");
-       separadores.add("Titulaciones");
-        profundidadElemento2.add("div.entry-content");
+       profundidadElemento1.add("div.panel-panel.col-md-9.col-xs-12.lastUnit.col-content.col-right");           
+                   
+//       etiquetasElementos.add("h3");
+//       tiposElementos.add("tag");
+//       separadores.add("Universidad");
+//       profundidadElemento1.add("div.entry-content");
 //       
-         etiquetasElementos.add("scr");
-       tiposElementos.add("attr");
-       separadores.add("<#>");
-       
-       profundidadElemento3.add("div.navigation-branding");
-       profundidadElemento3.add("div.sticky-navigation-logo");
-       profundidadElemento3.add("img.is-logo-image");
+//        etiquetasElementos.add("li");
+//       tiposElementos.add("tag");
+//       separadores.add("Titulaciones");
+//        profundidadElemento2.add("div.entry-content");
+//        
+
+        
+        
+//       
+//         etiquetasElementos.add("scr");
+//       tiposElementos.add("attr");
+//       separadores.add("<#>");
+//       
+//       profundidadElemento3.add("div.navigation-branding");
+//       profundidadElemento3.add("div.sticky-navigation-logo");
+//       profundidadElemento3.add("img.is-logo-image");
        
        profundidad.add(profundidadElemento1);
-         profundidad.add(profundidadElemento2);
-           profundidad.add(profundidadElemento3);
+//         profundidad.add(profundidadElemento2);
+//           profundidad.add(profundidadElemento3);
       resultado= scrp.obtenerTitulo(http,etiquetasElementos,tiposElementos,separadores,profundidad);
  
-      return resultado.get(0).get(2);
+      return resultado.get(0).get(0);
 
     }
     @GET
